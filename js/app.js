47bfacfa6877c24156de1e2c0f00a84ef3636cbc
Kit Kemper <cgkemper@gmail.com>
2014-11-17 14:25:52 -0500

    Added view_player. Added angular functionality to display player information.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-var app = angular.module('add_player', []);
+(function(){
+	var app = angular.module('add_player', []);
+
+	app.controller('PlayerController', function() {
+		this.people = players;
+	});
+
+	var players = [
+		{
+			fullname: 'Diego Costa',
+			shortname: 'Diego Costa',
+			club: 'Chelsea',
+			birthdate: '12/31/1985',
+		},
+		{
+			fullname: 'Eden Hazard',
+			shortname: 'Eden Hazard',
+			club: 'Chelsea',
+			birthdate: '1/1/1988',
+		},
+		{
+			fullname: 'Thibault Courtois',
+			shortname: 'Thibault Courtois',
+			club: 'Chelsea',
+			birthdate: '1/1/1988',
+		}
+	]
+
+
+
+
+})();
